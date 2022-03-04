9e71dc36f204a7df815cfa46a62062f8960f9194
a-camilo <acamilo10@hotmail.com>
2022-03-04 14:38:38 -0300

    Aula Comparable

---
+++ Comparable/src/application/Program2.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+        String path = "Comparable/in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String text = br.readLine();
+            while (text != null) {
+                System.out.println(text);
+                text = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
