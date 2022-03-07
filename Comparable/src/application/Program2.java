27f7c5c04946f26d7425cbde6cab5b3d785edb5a
a-camilo <acamilo10@hotmail.com>
2022-03-07 10:45:20 -0300

    Aula Generics

--- Comparable/src/application/Program2.java
+++ Comparable/src/application/Program2.java
@@ -3,8 +3,6 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Program2 {
 
---
+++ Generics/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import service.PrintService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+        for (int i = 0; i<n;i++){
+            Integer value = sc.nextInt();
+            ps.addValue(value);
+        }
+        ps.print();
+        System.out.println("First: " + ps.first());
+        sc.close();
+    }
+}
